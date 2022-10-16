26f81eaa023c8f9d4370e11e6ff72859e56a109e
Forkinel <bobscore@gmail.com>
2022-10-16 12:44:51 +0100

    Section 33 Complete - Created and saved a category

--- BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -22,10 +22,19 @@ namespace BulkyBookWeb.Controllers
 
         // GET 
         public IActionResult Create()
-        {
-          
-
+        {          
             return View();
+        }
+
+        // POST 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create( Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
